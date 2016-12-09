f2331bc847314600600b2dc21662c88dba8c0b72
ellsve <elliot.sverin@hotmail.com>
2016-12-09 09:44:24 +0100

    JS Tweens and timelines

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-TweenMax.from(".arrow-down", 0.5, {
-    ease: Expo.easeOut
-    , scaleX: 0
-    , scaleY: 0
-    , borderTopColor: "rgba(255, 255, 255, 0)"
-});
-TweenMax.to(".arrow-down", 0.5, {
-    bottom: "1"
-    , repeat: -1
-    , yoyo: true
-});
+function arrowBounce() {
+    TweenMax.to(".arrow-down", 0.5, {
+        bottom: "1"
+        , repeat: -1
+        , yoyo: true
+    });
+}
+var tl = new TimelineLite();
+tl.from(".textovervideo", 2.5, {
+    ease: Power2.easeOut
+    , autoAlpha: 0
+}).from(".arrow-down", 1, {
+    autoAlpha: 0
+    , bottom: -5
+}).call(arrowBounce);
